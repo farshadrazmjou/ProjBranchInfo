fcd41d57baaab88ae65f0fce3d58ba52b5a9bc20
farshad <farshad.razmjou@gmail.com>
2024-09-20 22:09:09 +0330

    develop test
    Test project
    Delete unit test

--- Entity/obj/Debug/net8.0/Entity.AssemblyInfo.cs
+++ Entity/obj/Debug/net8.0/Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abae6030ec6dd61443885547c6ce5e9ddc0ed6c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ff8a95b6c1798d6bf368b46c9d936034aaa687")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Test/Test/PlaceUnitTest.cs
+++ Test/Test/PlaceUnitTest.cs
@@ -1,3 +1,4 @@
+using System.Security.Principal;
 using Contract.DTO;
 using Service;
 
@@ -16,16 +17,16 @@ namespace Test
         {
             return new List<PlaceAddRequest>
             {
-                new PlaceAddRequest() {Name="Briano",Code=7693,Grade=2,Address="170 Roxbury Plaza"},
-                new PlaceAddRequest() {Name="Brenden",Code=7231,Grade=2,Address="5840 Fallview Lane"},
-                new PlaceAddRequest() {Name="Hasheem",Code=7214,Grade=1,Address="7 Melody Point"},
-                new PlaceAddRequest() {Name="Reggis",Code=7218,Grade=2,Address="76 Blaine Center"},
-                new PlaceAddRequest() {Name="Barnebas",Code=7564,Grade=3,Address="6 Susan Drive"},
-                new PlaceAddRequest() {Name="Marshal",Code=7669,Grade=1,Address="15 Meadow Valley Junction"},
-                new PlaceAddRequest() {Name="Basil",Code=7251,Grade=2,Address="97 Drewry Center"},
-                new PlaceAddRequest() {Name="Ned",Code=7390,Grade=3,Address="5 Glendale Place"},
-                new PlaceAddRequest() {Name="Raynard",Code=7268,Grade=2,Address="529 Packers Point"},
-                new PlaceAddRequest() {Name="Wittie",Code=7440,Grade=3,Address="99 Eastwood Center"}
+                new PlaceAddRequest() {Name="Shraz",Code=7693,Grade=2,Address="170 Roxbury Plaza"},
+                new PlaceAddRequest() {Name="Lar",Code=7231,Grade=2,Address="5840 Fallview Lane"},
+                new PlaceAddRequest() {Name="Lamerd",Code=7214,Grade=1,Address="7 Melody Point"},
+                new PlaceAddRequest() {Name="Kousar",Code=7218,Grade=2,Address="76 Blaine Center"},
+                new PlaceAddRequest() {Name="Zand",Code=7564,Grade=3,Address="6 Susan Drive"},
+                new PlaceAddRequest() {Name="Sadi",Code=7669,Grade=1,Address="15 Meadow Valley Junction"},
+                new PlaceAddRequest() {Name="Kazerun",Code=7251,Grade=2,Address="97 Drewry Center"},
+                new PlaceAddRequest() {Name="Jahrom",Code=7390,Grade=3,Address="5 Glendale Place"},
+                new PlaceAddRequest() {Name="Fasa",Code=7268,Grade=2,Address="529 Packers Point"},
+                new PlaceAddRequest() {Name="Abadeh",Code=7440,Grade=3,Address="99 Eastwood Center"}
             };
         }
 
@@ -95,6 +96,89 @@ namespace Test
 
             // Assert
             Assert.Equal(placeResponse_from_add,placeResponse_from_get);
+        }
+
+        #endregion
+
+        #region GetAll
+
+        [Fact]
+        public async Task GetAll_Successfully()
+        {
+            // Arrange
+            List<PlaceResponse> list_placeResponse_from_add=new List<PlaceResponse>();
+
+            foreach(PlaceAddRequest placeAddRequest in get_PlacesData())
+            {
+                list_placeResponse_from_add.Add(await _placeService.AddAsync(placeAddRequest));
+            }
+
+            // Act
+            List<PlaceResponse> list_placeResponse_from_getAll=await _placeService.GetAllAsync();
+
+            // Assert
+            for(int i=0;i<list_placeResponse_from_getAll.Count;i++)
+            {
+                Assert.Equal(list_placeResponse_from_add[i],list_placeResponse_from_getAll[i]);
+            }
+
+        }
+
+        #endregion
+
+        #region Delete
+
+        [Fact]
+        public async Task Delete_NullArgument()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(async ()=> {
+                await _placeService.DeleteAsync(null);
+            });
+        }
+
+        [Fact]
+        public async Task Delete_InvalidId()
+        {
+            // Arrange
+            PlaceAddRequest placeAddRequest=new PlaceAddRequest()
+            {
+                Name="Shiraz",
+                Address="shiraz zand street",
+                Code=7201,
+                Grade=1
+            };
+            PlaceResponse placeResponse_from_add= await _placeService.AddAsync(placeAddRequest);
+            
+            // Act
+            bool delete_result=await _placeService.DeleteAsync(Guid.NewGuid());
+            // Assert
+            Assert.False(delete_result);
+
+        }
+
+        [Fact]
+        public async Task Delete_Successfully()
+        {
+            // Arrange
+            PlaceAddRequest placeAddRequest=new PlaceAddRequest()
+            {
+                Name="Shiraz",
+                Address="shiraz zand street",
+                Code=7201,
+                Grade=1
+            };
+            PlaceResponse placeResponse_from_add= await _placeService.AddAsync(placeAddRequest);
+            
+            // Act
+            bool delete_result=await _placeService.DeleteAsync(placeResponse_from_add.Id);
+
+            // Assert
+            Assert.True(delete_result);
         }
 
         #endregion
